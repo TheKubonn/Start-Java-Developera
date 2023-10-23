2c65838b2db3ddc8a0dfb527a022e0fbef3ad116
Jakub Wojciechowski <thekubonn@hotmail.com>
2023-10-23 22:41:38 +0200

    First play with expressions

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,23 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.print("Hello World");
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.print("My Numbers Are: ");
+        System.out.print(myFirstNumber);
+        System.out.print(" ");
+        System.out.print(mySecondNumber);
+        System.out.print(" ");
+        System.out.println(myThirdNumber);
+        System.out.print("My total amount of the numbers is: ");
+        System.out.println(myTotal);
+
+        myThirdNumber = myFirstNumber * 2;
+        System.out.println(myThirdNumber);
+
+        myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
     }
 }
