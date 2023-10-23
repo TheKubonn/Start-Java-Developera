69e1eb9e34e5a6ca951ac327bac7b03c6c5eabd8
Jakub Wojciechowski <thekubonn@hotmail.com>
2023-10-23 22:49:18 +0200

    New file and 1st challenge

---
+++ src/ExpressionChallenge.java
@@ -0,0 +1,14 @@
+public class ExpressionChallenge {
+    public static void main(String[] args) {
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+
+        System.out.print("myTotal Value is: ");
+        System.out.println(myTotal);
+        System.out.print("myLastOne Value is: ");
+        System.out.print(myLastOne);
+    }
+}
