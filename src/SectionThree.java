1a07374ddc12563e3cf7be8e71eb01795e3287f5
Jakub Wojciechowski <thekubonn@hotmail.com>
2023-10-23 23:05:25 +0200

    Day two of learning

---
+++ src/SectionThree.java
@@ -0,0 +1,19 @@
+public class SectionThree {
+    public static void main(String[] args) {
+        // An integer is a whole number, meaning it doesn't contain a fractional element, or a decimal
+        // Every Primitive types got their minimal and maximum value
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        System.out.println("Integer min value: " + myMinIntValue);
+
+        // One way to display it
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer max value: " + myMaxIntValue);
+
+        // We don't need to assign it to any data type to display
+        System.out.println("Integer max value: " + Integer.MAX_VALUE);
+
+        System.out.println("Integer Value Range (" + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE + ")" );
+
+    }
+}
